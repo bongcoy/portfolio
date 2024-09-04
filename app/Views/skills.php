d3358463f9eb47a319e2e03d337787d9e23f9422
bongcoy <mpascaldewantara@gmail.com>
2024-09-05 01:47:07 +0700

    chore: Add skills page template

---
+++ app/Views/skills.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('content') ?>
+<h1>Skills</h1>
+<ul class="list-group">
+    <li class="list-group-item">PHP, CodeIgniter, Laravel</li>
+    <li class="list-group-item">MySQL, PostgreSQL</li>
+    <li class="list-group-item">JavaScript, jQuery, Bootstrap</li>
+    <li class="list-group-item">Python, Data Analysis, Pandas</li>
+</ul>
+<?= $this->endSection() ?>
