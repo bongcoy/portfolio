135066fb17583479d52d658280b246400d635a90
bongcoy <mpascaldewantara@gmail.com>
2024-09-05 01:47:12 +0700

    feat: Add projects page template and content

---
+++ app/Views/projects.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('content') ?>
+<h1>Projects</h1>
+<ul class="list-group">
+    <li class="list-group-item">Project 1 - Web Application Development</li>
+    <li class="list-group-item">Project 2 - Data Analytics Dashboard</li>
+    <li class="list-group-item">Project 3 - E-commerce Platform</li>
+</ul>
+<?= $this->endSection() ?>
